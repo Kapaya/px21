c52dc57664fc8f17ad5dd56c424ec862cd3d6bff
Geoffrey Litt <gklitt@gmail.com>
2021-02-13 12:27:23 -0500

    initial latex compile

--- compile-latex.sh
+++ compile-latex.sh
@@ -3,18 +3,17 @@ set -e
 pandoc \
   --filter conditional-render \
   --filter pandoc-crossref \
-  --filter pandoc-citeproc \
+    --citeproc \
   -s \
   -o \
   paper.tex \
-  --template=templates/pandoc-template-programming.pandoc \
-  --biblatex \
+  --template=templates/sigconf.pandoc \
+  --natbib \
   --metadata=format:pdf \
   paper.md
 
 pdflatex paper.tex
-biber paper
+bibtex paper
 pdflatex -interaction=batchmode paper.tex
 pdflatex -interaction=batchmode paper.tex
-cp paper.pdf ~/dev/homepage/source/wildcard/salon2020
 open paper.pdf
